3a33320f9f763fc5ad5b78c732412c3433984f9d
Haziq Hairil <haziqhairil21@gmail.com>
2023-06-11 17:38:57 +0800

    Image gallery height fix

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, presetUno, presetAttributify } from "unocss";
 
 export default defineConfig({
+  shortcuts: {
+    "mosaic-img": "bg-teal-700 rounded-lg shadow-lg",
+  },
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
