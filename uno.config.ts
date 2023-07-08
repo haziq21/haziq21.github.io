a222fd1ae65759316efcb091e6f1e14914444b07
Haziq Hairil <haziqhairil21@gmail.com>
2023-07-08 18:54:25 +0800

    Componentise navbar items

--- uno.config.ts
+++ uno.config.ts
@@ -2,12 +2,15 @@ import { defineConfig, presetUno, presetAttributify } from "unocss";
 
 export default defineConfig({
   shortcuts: {
-    "nav-even": "bg-amber-50 text-teal-900 fill-teal-900",
-    "nav-odd": "bg-teal-900 text-amber-50 fill-amber-50",
+    "nav-even": "bg-amber-50 text-teal-900 fill-teal-900 stroke-teal-900",
+    "nav-odd": "bg-teal-900 text-amber-50 fill-amber-50 stroke-teal-50",
   },
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
+    },
+    data: {
+      even: "color-scheme=even",
     },
   },
   presets: [presetUno(), presetAttributify()],
