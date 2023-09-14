a27389b703df73dd8b3981ef3447f0feed25f5e3
Haziq Hairil <haziqhairil21@gmail.com>
2023-09-14 20:28:45 +0800

    Remove color-changing on navbar

--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig, presetUno, presetAttributify } from "unocss";
+import { defineConfig, presetAttributify, presetUno } from "unocss";
 
 export default defineConfig({
-  shortcuts: {
-    "nav-even": "bg-amber-50 text-teal-900 fill-teal-900 stroke-teal-900",
-    "nav-odd": "bg-teal-900 text-amber-50 fill-amber-50 stroke-teal-50",
-  },
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
-    },
-    data: {
-      even: "color-scheme=even",
     },
   },
   presets: [presetUno(), presetAttributify()],
